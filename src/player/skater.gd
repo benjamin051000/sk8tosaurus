d25bc75533f90cd6ecc01b6e539b662719649b5e
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2026-07-24 21:47:43 -0400

    skater: Add coast & push sound effects
    Signed-off-by: Benjamin Wheeler <benjaminwheeler0510@gmail.com>

--- src/player/skater.gd
+++ src/player/skater.gd
@@ -6,11 +6,17 @@ extends Area2D
 @export var fall_time: float = 0.1
 
 var tween: Tween
+var jumping := false
 
 func jump() -> void:
+	jumping = true
 	if tween and tween.is_running():
 		return  # already jumping, ignore
 
+	$SkateSound.stop()
+	$Push1.stop()
+	$Push2.stop()
+	$PushSoundTimer.paused = true
 	var ground_y := position.y
 	
 	tween = create_tween()
@@ -22,8 +28,17 @@ func jump() -> void:
 	
 	tween.tween_property(self, "position:y", ground_y, fall_time) \
 		.set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)
+	tween.tween_callback($SkateSound.play)
+	tween.tween_callback(func(): $PushSoundTimer.paused = false)
 
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		jump()
+
+func _on_push_sound_timer_timeout() -> void:
+	var sounds = [$Push1, $Push2]
+	# TODO vary the pitch and volume?
+	sounds.pick_random().play()
+	# Vary the timer a bit
+	$PushSoundTimer.wait_time = randf_range(1.0, 3.0)
