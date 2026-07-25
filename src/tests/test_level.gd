e18143ce28df3fc2784e5eaa64f8f38d252f593d
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2026-07-25 17:04:31 -0400

    add cloud parallax
    Signed-off-by: Benjamin Wheeler <benjaminwheeler0510@gmail.com>

---
+++ src/tests/test_level.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var skater: Area2D = $Skater
+@onready var flat_level: Node2D = $FlatLevel
+
+@onready var back_clouds: Parallax2D = $"Back clouds"
+@onready var front_clouds: Parallax2D = $"Front Clouds"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	back_clouds.autoscroll.x = -300
+	front_clouds.autoscroll.x = -600
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
