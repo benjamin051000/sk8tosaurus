bec7a3094241f0bf1024a0f9032e84f61bdcd14d
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2026-07-26 11:11:29 -0400

    bug fix. spawn obstacles
    - Fix bug with obstacles looking for a y val when off left side of screen
    - spawn obstacles in dynamically
    
    Signed-off-by: Benjamin Wheeler <benjaminwheeler0510@gmail.com>

--- src/core/main.gd
+++ src/core/main.gd
@@ -2,23 +2,33 @@ extends Node2D
 
 @onready var main_menu: CenterContainer = $MainMenu
 
+var level_scene = preload("res://src/levels/flat_level.tscn")
+var level = level_scene.instantiate()
+var skater_scene = preload("res://src/player/skater.tscn")
+var skater = skater_scene.instantiate()
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Utils.start_game.connect(start)
 
 func start() -> void:
-	var level_scene = preload("res://src/levels/flat_level.tscn")
-	var level = level_scene.instantiate()
 	add_child(level)
-	var skater_scene = preload("res://src/player/skater.tscn")
-	var skater = skater_scene.instantiate()
+
 	skater.ground_path = level.get_path()
 	skater.position = Vector2(397.0, 1080)
 	add_child(skater)
 	$Music.play()
-	# TODO start shooting obstacles?
+	spawn_obstacles()
 	
 
+func spawn_obstacles() -> void:
+	var obstacle_scene := preload("res://src/obstacles/obstacle.tscn")
+	for i in 32:
+		var obs := obstacle_scene.instantiate()
+		obs.beats_away = i
+		obs.ground_path = level.get_path()
+		obs.skater_path = skater.get_path()
+		$Obstacles.add_child(obs)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 	#if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
--- src/obstacles/obstacle.gd
+++ src/obstacles/obstacle.gd
@@ -4,7 +4,12 @@ extends Node2D
 ## (when this value reaches 0)
 @export var beats_away: float = 4.0
 @export var ground_path: NodePath
+@export var skater_path: NodePath
+
+## This is to just give a little leeway for the player
+@export var tiny_offset := 0
 var ground: Node2D
+var skater: Area2D
 
 const bpm := 83
 const beats_per_measure := 4
@@ -16,14 +21,16 @@ var time_left: float
 
 func _ready() -> void:
 	var secs_away = beats_away * sec_per_beat
-	position.x = secs_away * -speed + 32  # sprite offset
+	skater = get_node(skater_path)
+	position.x = skater.front_x_offset + tiny_offset + secs_away * -speed + 32  # sprite offset
 	ground = get_node(ground_path)
-	var ground_y = ground.get_ground_height(position.x)
-	position.y = ground_y - 32  # offset
-
 
 func _process(delta: float) -> void:
 	position.x += speed * delta
+	
+	if position.x >= 0 and position.x <= 1920 + $Sprite2D.texture.get_width():
+		var ground_y = ground.get_ground_height(position.x)
+		position.y = ground_y - 32  # offset
 	
 	if position.x + $Sprite2D.texture.get_width() / 2 < 0:
 		queue_free()
